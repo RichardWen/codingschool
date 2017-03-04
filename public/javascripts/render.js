b333f475673045bc4081b938c7a8afcadb7460d9
Aman Agarwal <amanaamazing@gmail.com>
2017-03-04 14:21:24 -0800

    processing ball moving demo works now with only canvas and only js editor

--- public/javascripts/render.js
+++ public/javascripts/render.js
@@ -1,59 +1,24 @@
-var html_editor = document.querySelector('#html textarea'),
-    css_editor = document.querySelector('#css textarea'),
-    js_editor = document.querySelector('#js textarea');
+var js_editor = document.querySelector('#js textarea');
+var p;
 
-
-var editors = [html_editor, css_editor, js_editor];
-
-// Attaching the onkeyup Event
-editors.forEach(function(editor, i, arr) {
-    var button = document.getElementById("compile");
-    button.addEventListener('click', function() {
-        // The function that'll prepare the code and inject
-        // into the iframe.
-        render();
-    }, false);
-
-});
-
-// Base template
-var base_tpl =
-    "<!doctype html>\n" +
-    "<html>\n\t" +
-    "<head>\n\t\t" +
-    "<meta charset=\"utf-8\">\n\t\t" +
-    "<title>Test</title>\n\n\t\t\n\t" +
-    "</head>\n\t" +
-    "<body>\n\t\n\t" +
-    "</body>\n" +
-    "</html>";
+var button = document.getElementById("compile");
+button.addEventListener('click', function() {
+    if (p && p.exit) {
+        p.exit();
+    }
+    render();
+}, false);
 
 var prepareSource = function() {
-    var html = html_editor.value,
-        css = css_editor.value,
-        js = js_editor.value,
-        src = '';
-
-    // HTML
-    src = base_tpl.replace('</body>', html + '</body>');
-
-    // CSS
-    css = '<style>' + css + '</style>';
-    src = src.replace('</head>', css + '</head>');
-
-    // Javascript
-    js = '<script>' + js + '</script>';
-    src = src.replace('</body>', js + '</body>');
-
-    return src;
+    var js = js_editor.value;
+    return js;
 };
 
 var render = function() {
     var source = prepareSource();
-
-    var iframe = document.querySelector('#output iframe'),
-        iframe_doc = iframe.contentDocument;
-    iframe_doc.open();
-    iframe_doc.write(source);
-    iframe_doc.close();
+    function sketchProc(processing) {
+        eval(source);
+    }
+    var canvas = document.getElementById("canvas1");
+    p = new Processing(canvas, sketchProc);
 };
